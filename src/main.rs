04541372c85e191b85c2d5f84d348a4e6ee48ad6
Chris Wilhelm <cwilhelm@avvo.com>
2016-08-26 16:40:34 -0700

    contrast filter

--- src/main.rs
+++ src/main.rs
@@ -8,10 +8,38 @@ fn main() {
     let img = image::open(&Path::new("popeye.jpg")).unwrap();
 
     let bright_buffer: image::RgbImage = brighten(&img, 100);
+    let contrast_buffer: image::RgbImage = contrast(&img, 1.5);
 
     let _ = bright_buffer.save("brighter.jpg").unwrap();
-
+    let _ = contrast_buffer.save("contrasty.jpg").unwrap();
 }
+
+fn contrast(img: &DynamicImage, factor: f32) -> image::RgbImage {
+    let (width, height) = img.dimensions();
+    let mut buffer: image::RgbImage = ImageBuffer::new(width, height);
+
+    for pixel in img.pixels() {
+        match pixel {
+            (x, y, pixel) => buffer.put_pixel(x, y, Rgb{data: [
+                apply_contrast(*pixel.data.get(0).unwrap(), factor),
+                apply_contrast(*pixel.data.get(1).unwrap(), factor),
+                apply_contrast(*pixel.data.get(2).unwrap(), factor)
+            ]})
+        }
+    }
+
+    buffer
+}
+
+fn apply_contrast(value: u8, factor: f32) -> u8 {
+    let new_value = value as f32 * factor;
+    if new_value > (!0 as u8) as f32 {
+        !0 as u8
+    } else {
+        new_value as u8
+    }
+}
+
 
 fn brighten(img: &DynamicImage, steps: u8) -> image::RgbImage {
     let (width, height) = img.dimensions();
